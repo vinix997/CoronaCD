04f6210544e5dc55a629fc13a3ecd812d4ebc6bf
Chandra Delon <34028244+vinix997@users.noreply.github.com>
2020-03-27 15:05:00 +0700

    remove redundant

--- app/src/main/java/com/example/coronacd/CoronaDetailResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.coronacd;
-
-import java.util.List;
-
-public class CoronaDetailResponse {
-    private List<CoronaResponse> attribute;
-//    public List<CoronaResponse> getResults() { return results; }
-}
